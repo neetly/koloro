2ffc3f0ca129cdd5a79a9e774e8d60d2b60b4fb5
Hikari Hayashi <rev.hikari@gmail.com>
2022-12-02 14:42:27 +0800

    Update colorjs.io defaults

--- packages/koloro/src/index.ts
+++ packages/koloro/src/index.ts
@@ -6,4 +6,7 @@ import { KoloroLCh } from "./colorspaces/KoloroLch";
 Color.Space.register(KoloroLab);
 Color.Space.register(KoloroLCh);
 
+Color.defaults.gamut_mapping = "koloro-lch.c";
+Color.defaults.interpolationSpace = "koloro-lch";
+
 export { Color };
