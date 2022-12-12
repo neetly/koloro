420491da1205110ec87bd1af748844bc1cebe55f
Hikari Hayashi <rev.hikari@gmail.com>
2022-12-13 01:55:51 +0800

    Upgrade colorjs.io

--- packages/koloro/src/index.ts
+++ packages/koloro/src/index.ts
@@ -1,5 +1,4 @@
 import Color from "colorjs.io";
-import type { DefineFunctionHybrid } from "colorjs.io/types/src/color";
 
 import { KoloroLab } from "./colorspaces/KoloroLab";
 import { KoloroLCh } from "./colorspaces/KoloroLch";
@@ -8,9 +7,7 @@ import { deltaEKoloro } from "./deltaE/deltaEKoloro";
 Color.Space.register(KoloroLab);
 Color.Space.register(KoloroLCh);
 
-Color.defineFunctions({
-  deltaEKoloro: deltaEKoloro as unknown as DefineFunctionHybrid,
-});
+Color.defineFunctions({ deltaEKoloro });
 
 Color.defaults.gamut_mapping = "koloro-lch.c";
 Color.defaults.interpolationSpace = "koloro-lch";
